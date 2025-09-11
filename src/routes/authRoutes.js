88322189cd27ab7571fdfa8ce7d73c7c4145abd3
akhilva234 <akhilva10a@gmail.com>
2025-09-11 12:38:27 +0530

    started doing db schema

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import express from 'express'
+import prisma from '../prismaClient.js'
 
 const router = express.Router()
 
 
 router.post('/register',(req,res)=>{
+   
 })
 
 export default router
